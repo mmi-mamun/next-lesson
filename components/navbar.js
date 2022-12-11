fa5f3ff8d42f47e84182e499de055093cde642f5
niloy143 <niloymahmudapu@gmail.com>
2022-12-11 16:54:59 +0600

    checkout route in the navbar

--- components/navbar.js
+++ components/navbar.js
@@ -7,19 +7,20 @@ const Navbar = () => {
 
     const menus = <>
         <li><Link href="/home" className={`mx-0.5 ${route === '/home' && "bg-primary"}`} >Home</Link></li>
+        <li><Link href="/checkout" className={`mx-0.5 ${route === '/checkout' && "bg-primary"}`} >Checkout</Link></li>
         <li><Link href="/categories" className={`mx-0.5 ${route === '/categories' && "bg-primary"}`} >Categories</Link></li>
         <li><Link href="/teachers" className={`mx-0.5 ${route === '/teachers' && "bg-primary"}`} >Teachers</Link></li>
         <li><Link href="/contact" className={`mx-0.5 ${route === '/contacts' && "bg-primary"}`} >Contact</Link></li>
         <li><Link href="/about" className={`mx-0.5 ${route === '/about' && "bg-primary"}`} >About</Link></li>
-        <li><button className="bg-[blue] text-white active:scale-90 transition uppercase font-semibold mx-0.5">Login</button></li>
-        <li><button className="bg-[blue] text-white active:scale-90 transition uppercase font-semibold mx-0.5">Register</button></li>
+        <li><button className="bg-[blue] text-white active:scale-90 transition uppercase font-medium mx-0.5">Login</button></li>
+        <li><button className="bg-[blue] text-white active:scale-90 transition uppercase font-medium mx-0.5">Register</button></li>
     </>
     return (
         <div className="navbar bg-base-100 shadow-md sticky top-0" style={{ justifyContent: 'space-between' }}>
             <div className="navbar-start" style={{ width: 'auto' }}>
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-primary lg:hidden mr-2">
-                        <BiMenu className="text-4xl" />
+                        <BiMenu className="text-2xl" />
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow gap-1 bg-base-100 rounded-box w-52">
                         {menus}
