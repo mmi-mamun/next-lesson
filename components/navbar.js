36fc7d85f170b021fc27d0590e829cc6579a3902
mmi-mamun <muzahidulislam735@gmail.com>
2022-12-11 13:11:26 +0600

    *

---
+++ components/navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <>
+            <div className="navbar bg-base-100">
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
