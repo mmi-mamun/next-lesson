711c4c3498b8f1716ea6073f67352ba5a5dd203b
Mihir160 <mihir.roy266@gmail.com>
2022-12-12 13:34:36 +0600

    teachers added

--- pages/teachers/[teacherId].js
+++ pages/teachers/[teacherId].js
@@ -1,12 +1,26 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import MainLayout from "../../layouts/mainLayout";
 
 const Teacher = () => {
     const { query: { teacherId } } = useRouter();
     console.log(teacherId)
+    useEffect(()=>{
+        if(teacherId){
+            fetch(`/api/teacher/${teacherId}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        }
+    },[teacherId])
+
     return (
         <div>
+            <MainLayout>
             this is Teacher {teacherId}
             <h1>I am ready to programming</h1>
+
+            </MainLayout>
+           
         </div>
     );
 }
